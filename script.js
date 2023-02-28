91c84e991573ca27f60bef5c7e71d87db5d947cf
Kinga Woch <kinga-niecko@o2.pl>
2023-02-28 09:23:50 +0100

    mobile dropdown menu

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const dropDowns = document.querySelectorAll(".dropdown");
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.querySelector(".header-menu");
 
 dropDowns.forEach((dropDown) =>
   dropDown.addEventListener("click", () => {
     if (dropDown.classList.contains("active")) {
       dropDown.classList.toggle("active");
     } else {
-      dropDowns.forEach((dropDown) => dropDown.classList.remove("active"));
       dropDown.classList.add("active");
     }
   })
 );
+
+menuBtn.addEventListener("click", () => {
+  if (menuBtn.classList.contains("open")) {
+    menuBtn.classList.remove("open");
+    menu.classList.remove("visible");
+  } else {
+    menuBtn.classList.add("open");
+    menu.classList.add("visible");
+  }
+});
